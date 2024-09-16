0b3b2075f162b5004af80d2378aeace913636dda
Ndiritu <ndiritumichael5@gmail.com>
2024-09-17 00:19:13 +0300

    compose project setup

---
+++ build-logic/convention/src/main/kotlin/com/devmike/gitissuesmobile/AndroidCompose.kt
@@ -0,0 +1,24 @@
+package com.devmike.gitissuesmobile
+
+import com.android.build.api.dsl.CommonExtension
+import org.gradle.api.Project
+import org.gradle.kotlin.dsl.dependencies
+
+internal fun Project.configureAndroidCompose(commonExtension: CommonExtension<*, *, *, *, *, *>) {
+    commonExtension.apply {
+        buildFeatures {
+            compose = true
+        }
+
+        dependencies {
+            val bom = libs.findLibrary("androidx-compose-bom").get()
+            add("implementation", platform(bom))
+            add("androidTestImplementation", platform(bom))
+            add("testImplementation", platform(bom))
+            add("androidTestImplementation", libs.findLibrary("androidx.ui.test.junit4").get())
+            add("implementation", libs.findBundle("compose").get())
+            add("debugImplementation", libs.findLibrary("androidx.ui.test.manifest").get())
+            add("testImplementation", libs.findLibrary("androidx.ui.test.junit4").get())
+        }
+    }
+}
---
+++ build-logic/convention/src/main/kotlin/com/devmike/gitissuesmobile/KotlinAndroid.kt
@@ -0,0 +1,31 @@
+package com.devmike.gitissuesmobile
+
+import com.android.build.api.dsl.CommonExtension
+import org.gradle.api.JavaVersion
+import org.gradle.api.Project
+import org.gradle.kotlin.dsl.dependencies
+
+internal fun Project.configureKotlinAndroid(commonExtension: CommonExtension<*, *, *, *, *, *>) {
+    commonExtension.apply {
+        compileSdk = 34
+
+        defaultConfig {
+            minSdk = 26
+        }
+
+        compileOptions {
+            sourceCompatibility = JavaVersion.VERSION_17
+            targetCompatibility = JavaVersion.VERSION_17
+        }
+
+        dependencies {
+            add("implementation", libs.findLibrary("timber").get())
+            add("implementation", libs.findLibrary("androidx.core.ktx").get())
+
+            add("androidTestImplementation", libs.findLibrary("androidx.espresso.core").get())
+            add("androidTestImplementation", libs.findLibrary("androidx.junit").get())
+
+            add("testImplementation", libs.findLibrary("junit").get())
+        }
+    }
+}
