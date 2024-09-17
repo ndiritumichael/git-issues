92e772bb78691f001b68a432e1bb689f2742bc26
Ndiritu <ndiritumichael5@gmail.com>
2024-09-17 09:21:40 +0300

    Feature convention plugin  setup

---
+++ build-logic/convention/src/main/kotlin/AndroidFeatureConventionPlugin.kt
@@ -0,0 +1,33 @@
+
+
+import com.android.build.gradle.LibraryExtension
+import com.devmike.gitissuesmobile.libs
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+import org.gradle.kotlin.dsl.configure
+import org.gradle.kotlin.dsl.dependencies
+
+class AndroidFeatureConventionPlugin : Plugin<Project> {
+    override fun apply(target: Project) {
+        with(target) {
+            pluginManager.apply {
+                apply("gitissuesmobile.android.library")
+                apply("gitissuesmobile.android.library.compose")
+                apply("gitissuesmobile.android.hilt")
+            }
+            extensions.configure<LibraryExtension> {
+                defaultConfig {
+                    testInstrumentationRunner =
+                        "androidx.test.runner.AndroidJUnitRunner"
+                }
+                testOptions.animationsDisabled = true
+            }
+
+            dependencies {
+                add("implementation", libs.findLibrary("androidx.hilt.navigation.compose").get())
+                add("implementation", libs.findLibrary("androidx.lifecycle.runtimeCompose").get())
+                add("implementation", libs.findLibrary("androidx.lifecycle.viewmodel.compose").get())
+            }
+        }
+    }
+}
