9795bbfc089e332b085a2cfa5bde7a0044d5f008
Ndiritu <ndiritumichael5@gmail.com>
2024-09-21 21:20:17 +0300

    create issue dao create issue entity

--- core/database/src/main/java/com/devmike/database/GithubDatabase.kt
+++ core/database/src/main/java/com/devmike/database/GithubDatabase.kt
@@ -2,7 +2,12 @@ package com.devmike.database
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import com.devmike.database.dao.IssueKeyEntityDao
+import com.devmike.database.dao.IssuesDao
+import com.devmike.database.dao.RepoRemoteKeysDao
 import com.devmike.database.dao.RepositorySearchDAO
+import com.devmike.database.entities.CachedIssueEntity
+import com.devmike.database.entities.CachedIssueKeyEntity
 import com.devmike.database.entities.CachedRepository
 import com.devmike.database.entities.RemoteKeyEntity
 
@@ -10,10 +15,18 @@ import com.devmike.database.entities.RemoteKeyEntity
     entities = [
         CachedRepository::class,
         RemoteKeyEntity::class,
+        CachedIssueEntity::class,
+        CachedIssueKeyEntity::class,
     ],
     version = 1,
     exportSchema = true,
 )
 abstract class GithubDatabase : RoomDatabase() {
     abstract fun getRepositoryDao(): RepositorySearchDAO
+
+    abstract fun remoteKeyDao(): RepoRemoteKeysDao
+
+    abstract fun issueDao(): IssuesDao
+
+    abstract fun issueKeyDao(): IssueKeyEntityDao
 }
--- core/database/src/main/java/com/devmike/database/di/DbModule.kt
+++ core/database/src/main/java/com/devmike/database/di/DbModule.kt
@@ -3,7 +3,14 @@ package com.devmike.database.di
 import android.content.Context
 import androidx.room.Room
 import com.devmike.database.GithubDatabase
+import com.devmike.database.dao.IssueKeyEntityDao
+import com.devmike.database.dao.IssuesDao
+import com.devmike.database.dao.RepoRemoteKeysDao
 import com.devmike.database.dao.RepositorySearchDAO
+import com.devmike.database.repository.CachedIssueRepo
+import com.devmike.database.repository.CachedIssueRepoImpl
+import com.devmike.database.repository.CachedRepoSearch
+import com.devmike.database.repository.CachedRepoSearchImpl
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -29,4 +36,24 @@ object DbModule {
     @Singleton
     @Provides
     fun providesRepositoryDao(db: GithubDatabase): RepositorySearchDAO = db.getRepositoryDao()
+
+    @Singleton
+    @Provides
+    fun providesRemoteKeyDao(db: GithubDatabase): RepoRemoteKeysDao = db.remoteKeyDao()
+
+    @Singleton
+    @Provides
+    fun providesIssueDao(db: GithubDatabase): IssuesDao = db.issueDao()
+
+    @Singleton
+    @Provides
+    fun providesCachedRepo(db: GithubDatabase): CachedRepoSearch = CachedRepoSearchImpl(db)
+
+    @Singleton
+    @Provides
+    fun providesIssueKeysDao(db: GithubDatabase): IssueKeyEntityDao = db.issueKeyDao()
+
+    @Singleton
+    @Provides
+    fun providescachedIssueRepo(db: GithubDatabase): CachedIssueRepo = CachedIssueRepoImpl(db)
 }
