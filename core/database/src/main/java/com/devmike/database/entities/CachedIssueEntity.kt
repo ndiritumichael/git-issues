4f5328aa42f226a3070ea77ae95fbc55d432ef04
Ndiritu <ndiritumichael5@gmail.com>
2024-09-24 23:38:21 +0300

    normalize assignees and labels to their own tables

--- core/database/src/main/java/com/devmike/database/entities/CachedIssueEntity.kt
+++ core/database/src/main/java/com/devmike/database/entities/CachedIssueEntity.kt
@@ -1,7 +1,10 @@
 package com.devmike.database.entities
 
+import androidx.room.Embedded
 import androidx.room.Entity
+import androidx.room.Index
 import androidx.room.PrimaryKey
+import androidx.room.Relation
 
 @Entity(tableName = "issues")
 data class CachedIssueEntity(
@@ -12,8 +15,36 @@ data class CachedIssueEntity(
     val url: String,
     val title: String,
     val createdAt: String,
-    val label: String,
     val author: String,
     val repositoryName: String,
-    val assignee: String? = null,
+)
+
+data class IssueWithAssigneesAndLabels(
+    @Embedded val issue: CachedIssueEntity,
+    @Relation(
+        parentColumn = "id",
+        entityColumn = "issueId",
+    )
+    val assignees: List<AssigneeEntity>,
+    @Relation(
+        parentColumn = "id",
+        entityColumn = "issueId",
+    )
+    val labels: List<LabelEntity>,
+)
+
+@Entity(tableName = "assignees")
+data class AssigneeEntity(
+    @PrimaryKey(autoGenerate = true)
+    val id: Int = 0,
+    val issueId: String,
+    val assignee: String,
+)
+
+@Entity(tableName = "labels", indices = [Index(value = ["issueId", "label"], unique = true)])
+data class LabelEntity(
+    @PrimaryKey(autoGenerate = true)
+    val id: Int = 0,
+    val issueId: String,
+    val label: String,
 )
