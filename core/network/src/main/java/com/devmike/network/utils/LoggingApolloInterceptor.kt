604f067b88cc62fcb1f25d67e6c08931d43672df
Ndiritu <ndiritumichael5@gmail.com>
2024-09-28 09:55:18 +0300

    add LoggingApolloInterceptor interceptor

---
+++ core/network/src/main/java/com/devmike/network/utils/LoggingApolloInterceptor.kt
@@ -0,0 +1,19 @@
+package com.devmike.network.utils
+
+import com.apollographql.apollo.api.ApolloRequest
+import com.apollographql.apollo.api.ApolloResponse
+import com.apollographql.apollo.api.Operation
+import com.apollographql.apollo.interceptor.ApolloInterceptor
+import com.apollographql.apollo.interceptor.ApolloInterceptorChain
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.onEach
+
+class LoggingApolloInterceptor : ApolloInterceptor {
+    override fun <D : Operation.Data> intercept(
+        request: ApolloRequest<D>,
+        chain: ApolloInterceptorChain,
+    ): Flow<ApolloResponse<D>> =
+        chain.proceed(request).onEach { response ->
+            println("Received response for ${request.operation.name()}: ${response.data}")
+        }
+}
