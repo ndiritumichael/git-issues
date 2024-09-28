9bc7b395ca89a8a987a40eae3264621d5752e4af
Ndiritu <ndiritumichael5@gmail.com>
2024-09-28 09:57:06 +0300

    Implement issue data fetching

--- core/database/src/main/java/com/devmike/database/dao/models/CachedIssueDTO.kt
+++ core/database/src/main/java/com/devmike/database/dao/models/CachedIssueDTO.kt
@@ -1,7 +1,10 @@
 package com.devmike.database.dao.models
 
 data class CachedIssueDTO(
-    val assignee: String? = null,
+    val assignee: List<String> = emptyList(),
     val labels: List<String> = emptyList(),
     val repository: String,
+    val sortBy: String? = null,
+    val query: String?,
+    val issueStatus: String,
 )
