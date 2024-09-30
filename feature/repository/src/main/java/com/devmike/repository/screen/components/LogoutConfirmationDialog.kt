0f69b6fe7f9d1a09946636222fc64c8646eec9d4
Ndiritu <ndiritumichael5@gmail.com>
2024-09-30 20:15:38 +0300

    preview for logout dialog

--- feature/repository/src/main/java/com/devmike/repository/screen/components/LogoutConfirmationDialog.kt
+++ feature/repository/src/main/java/com/devmike/repository/screen/components/LogoutConfirmationDialog.kt
@@ -4,7 +4,12 @@ import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
 
 @Composable
 fun LogoutConfirmationDialog(
@@ -29,4 +34,15 @@ fun LogoutConfirmationDialog(
             },
         )
     }
+}
+
+@Composable
+@Preview(
+    showBackground = true,
+    backgroundColor = 0xFFF0EAE2,
+    device = "id:pixel_6",
+)
+fun LogoutConfirmationPreview() {
+    var showDialog by remember { mutableStateOf(true) }
+    LogoutConfirmationDialog(showDialog = showDialog, onDismiss = {}, onLogout = {})
 }
