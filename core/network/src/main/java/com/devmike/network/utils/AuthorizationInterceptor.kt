9db18d60bd3a6d66b982dcbaece75bf61421aae5
Ndiritu <ndiritumichael5@gmail.com>
2024-10-09 23:31:52 +0300

    interceptor doc

--- core/network/src/main/java/com/devmike/network/utils/AuthorizationInterceptor.kt
+++ core/network/src/main/java/com/devmike/network/utils/AuthorizationInterceptor.kt
@@ -8,6 +8,16 @@ import com.devmike.datastore.repo.DataStoreRepo
 import kotlinx.coroutines.flow.first
 import javax.inject.Inject
 
+/**
+ * An [HttpInterceptor] implementation that adds an "Authorization" header to outgoing GraphQL requests.
+ * If the user's token is available in the [DataStoreRepo], it is used to construct the header.
+ * If the response status code indicates an authentication error (401 or 403), the user's token is
+ * cleared from the [DataStoreRepo].
+ *
+ * @param dataStoreRepo The repository used to retrieve and clear the user's token.
+ * These response codes indicate that the client is not permitted.
+ * [Autherrors](https://github.com/graphql/graphql-over-http/blob/main/spec/GraphQLOverHTTP.md#:~:text=If%20the%20request%20is%20not,or%20similar%20appropriate%20status%20code.)
+ */
 class AuthorizationInterceptor
     @Inject
     constructor(
