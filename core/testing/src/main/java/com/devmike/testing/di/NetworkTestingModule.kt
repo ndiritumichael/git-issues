b733ced28cec04c962ad3fc92bc4eeed3ea3f0b2
Ndiritu <ndiritumichael5@gmail.com>
2024-10-11 23:51:54 +0300

    replace apollo client with a fake network module for tests

--- core/testing/src/main/java/com/devmike/testing/di/NetworkTestingModule.kt
+++ core/testing/src/main/java/com/devmike/testing/di/NetworkTestingModule.kt
@@ -1,4 +1,18 @@
 package com.devmike.testing.di
 
+import com.devmike.network.di.NetworkModule
+import com.devmike.network.networkSource.GitHubIssuesRepo
+import com.devmike.testing.networktestdoubles.FakeGithubRepo
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.components.SingletonComponent
+import dagger.hilt.testing.TestInstallIn
+import javax.inject.Singleton
+
+@Module
+@TestInstallIn(replaces = [NetworkModule::class], components = [SingletonComponent::class])
 object NetworkTestingModule {
+    @Provides
+    @Singleton
+    fun provideGitHubIssuesRepo(): GitHubIssuesRepo = FakeGithubRepo()
 }
