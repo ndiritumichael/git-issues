963b53b6fdf987ca0965ce882f3c1b9e4ee34849
Ndiritu <ndiritumichael5@gmail.com>
2024-10-18 14:38:19 +0300

    code cleanup

--- app/src/main/java/com/devmike/gitissuesmobile/MainActivity.kt
+++ app/src/main/java/com/devmike/gitissuesmobile/MainActivity.kt
@@ -117,7 +117,7 @@ class MainActivity : ComponentActivity() {
                                     "Something went wrong please try again",
                                     Toast.LENGTH_SHORT,
                                 ).show()
-                        } else{
+                        } else {
                             res?.accessToken?.let { insertToken(it) }
                         }
                     }
